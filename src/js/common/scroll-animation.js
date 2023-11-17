43c4a0898f5c3a5cb835ad84fd77ea3339aaef88
AnastasiaBar <90132016+AnastasiaBar@users.noreply.github.com>
2023-11-17 16:43:12 +0300

    feat: верстка первых двух секций

--- src/js/common/scroll-animation.js
+++ src/js/common/scroll-animation.js
@@ -4,53 +4,34 @@ export default function scrollPage() {
   gsap.registerPlugin(ScrollTrigger);
   const horizontalSections = gsap.utils.toArray(".horizontal-animation");
 
-  resizeWindow(window.innerWidth);
+  horizontalSections.forEach((section) => {
+    const thisPinWrap = section.querySelector(".horizontal-animation__inner");
+    const thisAnimWrap = thisPinWrap.querySelector(".horizontal-animation__animation");
+    const getToValue = () => -(thisAnimWrap.scrollWidth - window.innerWidth);
 
-  window.addEventListener("resize", function () {
-    let newWidth = window.innerWidth;
-    resizeWindow(newWidth);
+    gsap.fromTo(
+      thisAnimWrap,
+      {
+        x: () =>
+          thisAnimWrap.classList.contains("horizontal-animation__animation--right")
+            ? 0
+            : getToValue(),
+      },
+      {
+        x: () =>
+          thisAnimWrap.classList.contains("horizontal-animation__animation--right")
+            ? getToValue()
+            : 0,
+        ease: "none",
+        scrollTrigger: {
+          trigger: section,
+          pin: thisPinWrap,
+          invalidateOnRefresh: true,
+          scrub: true,
+          start: "top top",
+          end: () => "+=" + (thisAnimWrap.scrollWidth - window.innerWidth),
+        },
+      },
+    );
   });
-
-  function resizeWindow(width) {
-    if (width < 992) {
-      horizontalSections.forEach((section) => {
-        gsap.timeline({
-          scrollTrigger: {
-            trigger: section,
-            scrub: true,
-          },
-        });
-      });
-    } else {
-      horizontalSections.forEach((section) => {
-        const thisPinWrap = section.querySelector(".horizontal-animation__inner");
-        const thisAnimWrap = thisPinWrap.querySelector(".horizontal-animation__animation");
-        const getToValue = () => -(thisAnimWrap.scrollWidth - window.innerWidth);
-
-        gsap.fromTo(
-          thisAnimWrap,
-          {
-            x: () =>
-              thisAnimWrap.classList.contains("horizontal-animation__animation--right")
-                ? 0
-                : getToValue(),
-          },
-          {
-            x: () =>
-              thisAnimWrap.classList.contains("horizontal-animation__animation--right")
-                ? getToValue()
-                : 0,
-            ease: "none",
-            scrollTrigger: {
-              trigger: section,
-              pin: thisPinWrap,
-              invalidateOnRefresh: true,
-              scrub: true,
-              end: () => "+=" + (thisAnimWrap.scrollWidth - window.innerWidth),
-            },
-          },
-        );
-      });
-    }
-  }
 }
