eff19f972e3def60517991af62619ff6cad302b8
AnastasiaBar <90132016+AnastasiaBar@users.noreply.github.com>
2023-11-23 11:17:59 +0300

    fix: правки линий и скролла

--- src/blocks/about/about.js
+++ src/blocks/about/about.js
@@ -4,21 +4,30 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 import SplitType from "split-type";
 
 ready(function () {
-  let posStart = window.innerWidth < 1280 ? "top" : window.innerWidth;
-
-  gsap.registerPlugin(ScrollTrigger);
+  let posStart = window.innerWidth < 1280 ? "-200px" : window.innerWidth;
   const split = new SplitType(".about__text", { types: "line" });
 
-  split.lines.forEach((line) => {
-    gsap.to(line, {
-      backgroundPositionX: 0,
-      ease: "none",
-      scrollTrigger: {
-        trigger: line,
-        start: `${posStart} 25%`,
-        scrub: 1,
-        invalidateOnRefresh: true,
-      },
+  gsap.registerPlugin(ScrollTrigger);
+  settingsScrollText();
+
+  window.onresize = function () {
+    posStart = window.innerWidth < 1280 ? "-200px" : window.innerWidth;
+    split.split();
+    settingsScrollText();
+  };
+
+  function settingsScrollText() {
+    split.lines.forEach((line) => {
+      gsap.to(line, {
+        backgroundPositionX: 0,
+        ease: "none",
+        scrollTrigger: {
+          trigger: line,
+          start: `${posStart} 25%`,
+          scrub: 1,
+          invalidateOnRefresh: true,
+        },
+      });
     });
-  });
+  }
 });
