071d08eedc6a724b366f9207507be7d04bbd384f
AnastasiaBar <90132016+AnastasiaBar@users.noreply.github.com>
2023-11-23 09:07:14 +0300

    fix: правки по второму этапу

--- src/blocks/about/about.js
+++ src/blocks/about/about.js
@@ -1,34 +1,24 @@
 import ready from "../../js/utils/documentReady.js";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import SplitType from "split-type";
 
 ready(function () {
-  let posStart = window.innerWidth < 1280 ? "-300px" : window.innerWidth;
-
-  const textContent = document.querySelectorAll(".about__text");
-  textContent.forEach((item) => {
-    splitText(item, item.innerText.split(" "));
-  });
-
-  function splitText(wrapper, words) {
-    wrapper.innerHTML = "";
-    words.forEach((word) => {
-      wrapper.innerHTML += `<span>${word + " "}</span>`;
-    });
-  }
+  let posStart = window.innerWidth < 1280 ? "top" : window.innerWidth;
 
   gsap.registerPlugin(ScrollTrigger);
-  gsap.set(".about__text span", { opacity: 0.1 });
-  gsap.utils.toArray(".about__text span").forEach((span) => {
-    gsap.to(span, {
+  const split = new SplitType(".about__text", { types: "line" });
+
+  split.lines.forEach((line) => {
+    gsap.to(line, {
+      backgroundPositionX: 0,
+      ease: "none",
       scrollTrigger: {
-        trigger: span,
-        start: `${posStart} center`,
-        scrub: true,
+        trigger: line,
+        start: `${posStart} 25%`,
+        scrub: 1,
         invalidateOnRefresh: true,
       },
-      opacity: 1,
-      stagger: 0.1,
     });
   });
 });
